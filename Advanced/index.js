338a8fd1b1f3bbf7dec9aa7decb106f6f3b28ded
Max Strauss <max.strauss@gmx.at>
2020-06-09 18:02:51 +0200

    advanced exercise

---
+++ Advanced/index.js
@@ -0,0 +1,18 @@
+$("a[href]").attr('href', 'http://www.codefactory.wien/');
+
+$("code").css("color","red");
+
+$("ol > li").css("background-color", "#2a7b90");
+
+$("textarea").val("Express your opinion");
+
+$("img").attr("src", "https://codefactory.wien/wp-content/themes/hestia/cf-images/big-jpg/1-frontend.jpg");
+
+$("blockquote").css({"background-color": "orange", "font-style": "italic"});
+
+$("#logo").css("color", "black");
+$(".gray").css("color", "white");
+
+$(".date").remove();
+
+$("ul").append("<li><a href="http://all-free-download.com/free-website-templates">New Templates<a/></li> <li>Order Template</li> <li>Contact Us</li>");
